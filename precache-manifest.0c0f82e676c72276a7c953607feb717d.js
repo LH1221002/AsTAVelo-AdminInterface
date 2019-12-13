0e5d4ca33fd276e5f1336e413e3b9d04fbdc918f
Luis Herres <luis@herres.dev>
2019-12-13 13:47:12 +0100

    Updates

---
+++ precache-manifest.0c0f82e676c72276a7c953607feb717d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffd5f80625307e5b904e8d55431bc20",
+    "url": "/AsTAVelo-AdminInterface/index.html"
+  },
+  {
+    "revision": "ecebe59b30d336cad820",
+    "url": "/AsTAVelo-AdminInterface/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "9714a9fe5ae79e48ae23",
+    "url": "/AsTAVelo-AdminInterface/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "ecebe59b30d336cad820",
+    "url": "/AsTAVelo-AdminInterface/static/js/2.6b109b56.chunk.js"
+  },
+  {
+    "revision": "9714a9fe5ae79e48ae23",
+    "url": "/AsTAVelo-AdminInterface/static/js/main.2e97d9f5.chunk.js"
+  },
+  {
+    "revision": "a12a23720a2ede7e473a",
+    "url": "/AsTAVelo-AdminInterface/static/js/runtime~main.276d1a17.js"
+  },
+  {
+    "revision": "839eab34ec68b8ac414485dc54c1410a",
+    "url": "/AsTAVelo-AdminInterface/static/media/admin.839eab34.svg"
+  }
+]);
